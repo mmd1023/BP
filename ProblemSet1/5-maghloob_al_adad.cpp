067dce64da31e8254aff2affa62e4b2ac45f2400
MMD <mmd.gh313@gmail.com>
2025-11-02 15:22:43 +0330

    upgrade 5-maghloob_al_adad

--- ProblemSet1/5-maghloob_al_adad.cpp
+++ ProblemSet1/5-maghloob_al_adad.cpp
@@ -15,29 +15,15 @@ int main() {
         int digit[3] = {(n / 100) % 10, (n / 10) % 10, n % 10};
         if(digit[2] == digit[0])
             cout << n << " is a symmetrical number." << endl;
-        else {
-            int flag = 0;
-            for(int i = 2; i >= 0; i--) {
-                if(digit[i])
-                    flag = 1;
-                if(flag || digit[i])
-                    cout << digit[i];
-            }
-        }
+        else
+            cout << digit[2] * 100 + digit[1] * 10 + digit[0] << endl;
     }
     else {
         int digit[5] = {(n / 10000) % 10, (n / 1000) % 10, (n / 100) % 10, (n / 10) % 10, n % 10};
         if(digit[4] == digit[0] && digit[3] == digit[1])
             cout << n << " is a symmetrical number." << endl;
-        else {
-            int flag = 0;
-            for(int i = 4; i >= 0; i--) {
-                if(digit[i])
-                    flag = 1;
-                if(flag || digit[i])
-                    cout << digit[i];
-            }
-        }
+        else
+            cout << digit[4] * 10000 + digit[3] * 1000 + digit[2] * 100 + digit[1] * 10 + digit[0] << endl;
     }
     return 0;
 }
