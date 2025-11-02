8770a57f3130d1f6f204efe916f56548cb74e6a4
MMD <mmd.gh313@gmail.com>
2025-11-02 15:32:21 +0330

    uglify 7-mohasebe_hoghogh_karmandan

--- ProblemSet1/7-mohasebe_hoghogh_karmandan.cpp
+++ ProblemSet1/7-mohasebe_hoghogh_karmandan.cpp
@@ -9,19 +9,32 @@ using namespace std;
 int main() {
     ll employees[3][4];
 
-    for(int i = 0; i < 3; i++) {
-        for(int j = 0; j < 3; j++) {
-            cin >> employees[i][j];
-        }
-    }
+    cin >> employees[0][0];
+    cin >> employees[0][1];
+    cin >> employees[0][2];
+
+    cin >> employees[1][0];
+    cin >> employees[1][1];
+    cin >> employees[1][2];
+
+    cin >> employees[2][0];
+    cin >> employees[2][1];
+    cin >> employees[2][2];
 
     ll sum = 0;
-    for(int i = 0; i < 3; i++) {
-        employees[i][3] = employees[i][0] + 10000 * employees[i][1] + employees[i][2];
-        sum += employees[i][3];
 
-        cout << "Employee " << i+1 << " Final Salary: " << employees[i][3] << endl;
-    }
+    employees[0][3] = employees[0][0] + 10000 * employees[0][1] + employees[0][2];
+    sum += employees[0][3];
+    cout << "Employee " << 1 << " Final Salary: " << employees[0][3] << endl;
+
+    employees[1][3] = employees[1][0] + 10000 * employees[1][1] + employees[1][2];
+    sum += employees[1][3];
+    cout << "Employee " << 2 << " Final Salary: " << employees[1][3] << endl;
+
+    employees[2][3] = employees[2][0] + 10000 * employees[2][1] + employees[2][2];
+    sum += employees[2][3];
+    cout << "Employee " << 3 << " Final Salary: " << employees[2][3] << endl;
+
 
     cout << "Total Salaries: " << sum << endl;
     cout << "Average Salary: " << sum / 3 << endl;
