023a1135c6e139996e8e5ea4da646f7c12d991ab
MMD <mmd.gh313@gmail.com>
2025-11-02 15:21:41 +0330

    uglify 3-rezhim

--- ProblemSet1/3-rezhim.cpp
+++ ProblemSet1/3-rezhim.cpp
@@ -15,11 +15,22 @@ int main() {
     cin >> fruit >> amount;
 
     ll totual = 0;
-    for(int i = 0; i < 5; i++) {
-        if(fruit == fruits[i]) {
-            totual = amount * cal[i];
-        }
-    }
+    // for(int i = 0; i < 5; i++) {
+    //     if(fruit == fruits[i]) {
+    //         totual = amount * cal[i];
+    //     }
+    // }
+    if (fruit == fruits[0])
+        totual = amount * cal[0];
+    else if (fruit == fruits[1])
+        totual = amount * cal[1];
+    else if (fruit == fruits[2])
+        totual = amount * cal[2];
+    else if (fruit == fruits[3])
+        totual = amount * cal[3];
+    else if (fruit == fruits[4])
+        totual = amount * cal[4];
+
     cout << totual << endl;
 
     if(totual > 200) {
